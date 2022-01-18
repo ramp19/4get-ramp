1da3665863317c8f71e1230d52181f1704888fe3
ramp19 <rampingkurus897@gmail.com>
2022-01-18 15:37:24 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { DivisionComponent } from './division/division.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TambahDataComponent } from './tambah-data/tambah-data.component';
+import { EditDataComponent } from './edit-data/edit-data.component';
+import { HapusDataComponent } from './hapus-data/hapus-data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    DivisionComponent,
+    DashboardComponent,
+    AppFooterComponent,
+    TambahDataComponent,
+    EditDataComponent,
+    HapusDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
